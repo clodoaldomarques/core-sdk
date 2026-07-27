bfc36f02fdde88a8883d6375f4a5d36769098e5b
clodoaldomarques <clodoaldomarques@gmail.com>
2026-07-27 17:14:09 -0300

    correção do start do opentelemetry

--- pkg/opentelemetry/service.go
+++ pkg/opentelemetry/service.go
@@ -60,19 +60,7 @@ func InitMeter(ctx context.Context) *metric.MeterProvider {
 
 func Start(ctx context.Context) {
 	tracerProvicer := InitTracer(ctx)
-	defer func() {
-		if err := tracerProvicer.Shutdown(ctx); err != nil {
-			logger.Error(ctx, "error shutdown trace provider", logger.Fields{"error": err.Error()})
-		}
-	}()
-
 	meterProvicer := InitMeter(ctx)
-	defer func() {
-		if err := meterProvicer.Shutdown(ctx); err != nil {
-			logger.Error(ctx, "error shutdonw meter provider", logger.Fields{"error": err.Error()})
-		}
-	}()
-
 	otel.SetTracerProvider(tracerProvicer)
 	otel.SetMeterProvider(meterProvicer)
 }
